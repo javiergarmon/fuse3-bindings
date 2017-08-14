1ee451f8ea4e968d334a5ef2914aecf46f26d086
Javier Garmón <jgarmon@inevio.com>
2017-08-14 17:08:26 +0000

    Added more methods

--- fuse-bindings.cc
+++ fuse-bindings.cc
@@ -24,6 +24,7 @@
 
 using namespace v8;
 
+#define min(x, y) ((x) < (y) ? (x) : (y))
 #define LOCAL_STRING(s) Nan::New<String>(s).ToLocalChecked()
 #define LOCAL_NUMBER(i) Nan::New<Number>(i)
 #define LOOKUP_CALLBACK(map, name) map->Has(LOCAL_STRING(name)) ? new Nan::Callback(map->Get(LOCAL_STRING(name)).As<Function>()) : NULL
@@ -63,14 +64,17 @@ enum operations_constants {
   OP_SYMLINK,
   OP_MKDIR,
   OP_RMDIR,
-  OP_DESTROY
+  OP_DESTROY,
+  OP_LOOKUP
 };
 
 struct bindings_template {
   Nan::Callback *getattr;
-  Nan::Callback *stat;
-  Nan::Callback *readdir;
   Nan::Callback *lookup;
+  Nan::Callback *open;
+  Nan::Callback *read;
+  Nan::Callback *readdir;
+  Nan::Callback *release;
 };
 
 struct operation_template {
@@ -78,11 +82,12 @@ struct operation_template {
   int type;
   fuse_req_t *req;
   fuse_ino_t ino;
+  fuse_file_info *fi;
+  size_t size;
+  off_t offset;
+  const char *name;
+  char *data;
 };
-
-/*ToRemove*/ 
-static const char *hello_str = "Hello World!\n";
-static const char *hello_name = "hello";
 
 static uv_loop_t *loop;
 static uv_async_t loop_async;
@@ -90,6 +95,39 @@ static struct bindings_template bindings;
 static Nan::Callback *callback_constructor;
 static std::unordered_map<std::uint32_t, operation_template *> operations_map = {};
 static uint32_t operation_count = 0;
+static pthread_mutex_t mutex;
+static pthread_mutex_t mutex_ip;
+
+struct dirbuf {
+  char *p;
+  size_t size;
+};
+
+const char* ToConstString(const String::Utf8Value& value) {
+  return *value ? *value : "<string conversion failed>";
+}
+
+static void dirbuf_add( fuse_req_t req, struct dirbuf *b, const char *name, fuse_ino_t ino ){
+  struct stat stbuf;
+  size_t oldsize = b->size;
+  b->size += fuse_add_direntry(req, NULL, 0, name, NULL, 0);
+  b->p = (char *) realloc(b->p, b->size);
+  memset(&stbuf, 0, sizeof(stbuf));
+  stbuf.st_ino = ino;
+  fuse_add_direntry(req, b->p + oldsize, b->size - oldsize, name, &stbuf, b->size);
+}
+
+static int reply_buf_limited(fuse_req_t req, const char *buf, size_t bufsize, off_t off, size_t maxsize){
+
+  fprintf(stderr, "%s => %d - %d - %d\n", "reply_buf_limited", bufsize, off, maxsize);
+
+  if (off < bufsize){
+    return fuse_reply_buf(req, buf + off, min(bufsize - off, maxsize));
+  }else{
+    return fuse_reply_buf(req, NULL, 0);
+  }
+
+}
 
 NAN_INLINE static void bindings_set_date (struct timespec *out, Local<Date> date) {
   double ms = date->NumberValue();
@@ -102,13 +140,13 @@ NAN_INLINE static void bindings_set_date (struct timespec *out, Local<Date> date
 
 NAN_INLINE static void bindings_set_stat (struct stat *stat, Local<Object> obj) {
   if (obj->Has(LOCAL_STRING("dev"))) stat->st_dev = obj->Get(LOCAL_STRING("dev"))->NumberValue();
-  if (obj->Has(LOCAL_STRING("ino"))) stat->st_ino = obj->Get(LOCAL_STRING("ino"))->NumberValue();
+  if (obj->Has(LOCAL_STRING("ino"))) stat->st_ino = obj->Get(LOCAL_STRING("ino"))->Uint32Value();
   if (obj->Has(LOCAL_STRING("mode"))) stat->st_mode = obj->Get(LOCAL_STRING("mode"))->Uint32Value();
-  if (obj->Has(LOCAL_STRING("nlink"))) stat->st_nlink = obj->Get(LOCAL_STRING("nlink"))->NumberValue();
+  if (obj->Has(LOCAL_STRING("nlink"))) stat->st_nlink = obj->Get(LOCAL_STRING("nlink"))->Uint32Value();
   if (obj->Has(LOCAL_STRING("uid"))) stat->st_uid = obj->Get(LOCAL_STRING("uid"))->NumberValue();
   if (obj->Has(LOCAL_STRING("gid"))) stat->st_gid = obj->Get(LOCAL_STRING("gid"))->NumberValue();
   if (obj->Has(LOCAL_STRING("rdev"))) stat->st_rdev = obj->Get(LOCAL_STRING("rdev"))->NumberValue();
-  if (obj->Has(LOCAL_STRING("size"))) stat->st_size = obj->Get(LOCAL_STRING("size"))->NumberValue();
+  if (obj->Has(LOCAL_STRING("size"))) stat->st_size = obj->Get(LOCAL_STRING("size"))->Uint32Value();
   if (obj->Has(LOCAL_STRING("blocks"))) stat->st_blocks = obj->Get(LOCAL_STRING("blocks"))->NumberValue();
   if (obj->Has(LOCAL_STRING("blksize"))) stat->st_blksize = obj->Get(LOCAL_STRING("blksize"))->NumberValue();
 #ifdef __APPLE__
@@ -122,14 +160,36 @@ NAN_INLINE static void bindings_set_stat (struct stat *stat, Local<Object> obj) 
 #endif
 }
 
+#if (NODE_MODULE_VERSION > NODE_0_10_MODULE_VERSION && NODE_MODULE_VERSION < IOJS_3_0_MODULE_VERSION)
+NAN_INLINE v8::Local<v8::Object> bindings_buffer (char *data, size_t length) {
+  Local<Object> buf = Nan::New(buffer_constructor)->NewInstance(0, NULL);
+  Local<String> k = LOCAL_STRING("length");
+  Local<Number> v = LOCAL_NUMBER(length);
+  buf->Set(k, v);
+  buf->SetIndexedPropertiesToExternalArrayData((char *) data, kExternalUnsignedByteArray, length);
+  return buf;
+}
+#else
+void noop (char *data, void *hint) {}
+NAN_INLINE v8::Local<v8::Object> bindings_buffer (char *data, size_t length) {
+  fprintf(stderr, "%s => %d\n", "buffer nuevo", length);
+  return Nan::NewBuffer(data, length, noop, NULL).ToLocalChecked();
+}
+#endif
+
 NAN_METHOD(OpCallback){
 
+  pthread_mutex_lock(&mutex);
   operation_template *operation = operations_map[ info[ 0 ]->Uint32Value() ];
   operations_map.erase( operation->index );
-  int result = (info.Length() > 1 && info[1]->IsNumber()) ? info[1]->Uint32Value() : 0;
+  pthread_mutex_unlock(&mutex);
+
+  int result = (info.Length() > 1 && info[1]->IsNumber()) ? info[1]->Int32Value() : 0;
+
+  fprintf(stderr, "%s => %d\n", "op", result );
 
   if( operation->type == OP_GETATTR ){
-    
+
     if(result == 0 && info.Length() > 2 && info[2]->IsObject()){
 
       struct stat stbuf;
@@ -138,9 +198,82 @@ NAN_METHOD(OpCallback){
       fuse_reply_attr(*(operation->req), &stbuf, 1.0);
 
     }else{
+      fprintf(stderr, "%s %d\n", "get attr falla",ENOENT);
       fuse_reply_err(*(operation->req), ENOENT);
     }
 
+  }else if( operation->type == OP_LOOKUP ){
+
+    if(result == 0 && info.Length() > 2 && info[2]->IsObject()){
+
+      struct fuse_entry_param entry;
+
+      Local<Object> data = info[2].As<Object>();
+      uint32_t ino = data->Get(LOCAL_STRING("ino"))->Uint32Value();
+
+      memset(&entry, 0, sizeof(entry));
+      entry.ino = ino;
+      entry.attr_timeout = 1.0;
+      entry.entry_timeout = 1.0;
+      bindings_set_stat( &entry.attr, data);
+      fuse_reply_entry(*(operation->req), &entry);
+
+    }else{
+      fuse_reply_err(*(operation->req), result);
+    }
+
+  }else if( operation->type == OP_OPEN ){
+
+    if(result == 0 && info.Length() > 2 && info[2]->IsNumber()){
+      operation->fi->fh = info[2]->Uint32Value();
+      fuse_reply_open(*(operation->req), operation->fi);
+    }else{
+      fuse_reply_err(*(operation->req), result);
+    }
+
+  }else if( operation->type == OP_READDIR ){
+
+    if(result == 0 && info.Length() > 2 && info[2]->IsArray()){
+
+      struct dirbuf buf;
+
+      memset(&buf, 0, sizeof(buf));
+      dirbuf_add( *(operation->req), &buf, ".", 1);
+      dirbuf_add( *(operation->req), &buf, "..", 1);
+
+      Local<Array> entries = info[2].As<Array>();
+
+      Local<Object> entry;
+      Local<String> entryName;
+      uint32_t entryIno;
+
+      for( uint32_t i = 0; i < entries->Length(); i++ ){
+
+        entry = entries->Get( i )->ToObject();
+        entryName = entry->Get(LOCAL_STRING("name"))->ToString();
+        entryIno = entry->Get(LOCAL_STRING("ino"))->Uint32Value();
+
+        dirbuf_add( *(operation->req), &buf, ToConstString( String::Utf8Value( entryName ) ), entryIno );
+
+      }
+
+      reply_buf_limited( *(operation->req), buf.p, buf.size, operation->offset, operation->size );
+      free(buf.p);
+
+    }else{
+      fuse_reply_err(*(operation->req), result);
+    }
+
+  }else if( operation->type == OP_READ ){
+
+    if(result >= 0 ){
+      reply_buf_limited(*(operation->req), operation->data, result, operation->offset, operation->size);
+    }else{
+      fuse_reply_err(*(operation->req), result);
+    }
+
+  }else if( operation->type == OP_RELEASE ){
+    fuse_reply_err(*(operation->req), result);
   }else{
     fprintf(stderr, "OpCallback => %s\n", "not implemented");
   }
@@ -151,14 +284,47 @@ static void uv_handler(uv_async_t *handle){
   Nan::HandleScope scope;
 
   operation_template *operation = (operation_template *) handle->data;
+  pthread_mutex_unlock(&mutex_ip);
   Local<Value> tmp[] = { Nan::New<Number>(operation->index), Nan::New<FunctionTemplate>(OpCallback)->GetFunction() };
   Nan::Callback *callbackGenerator = new Nan::Callback(callback_constructor->Call(2, tmp).As<Function>());
   Local<Function> callback = callbackGenerator->GetFunction();
 
   if( operation->type == OP_GETATTR ){
-    
+
     Local<Value> tmp[] = {LOCAL_NUMBER(operation->ino), callback};
     bindings.getattr->Call( 2, tmp );
+
+  }else if( operation->type == OP_READDIR ){
+
+    Local<Value> tmp[] = {LOCAL_NUMBER(operation->ino), callback};
+    bindings.readdir->Call( 2, tmp );
+
+  }else if( operation->type == OP_LOOKUP ){
+
+    Local<Value> tmp[] = {LOCAL_NUMBER(operation->ino), LOCAL_STRING(operation->name), callback};
+    bindings.lookup->Call( 3, tmp );
+
+  }else if( operation->type == OP_OPEN ){
+
+    Local<Value> tmp[] = {LOCAL_NUMBER(operation->ino), LOCAL_NUMBER(operation->fi->fh), callback};
+    bindings.open->Call( 3, tmp );
+
+  }else if( operation->type == OP_READ ){
+
+    Local<Value> tmp[] = {
+      LOCAL_NUMBER(operation->ino),
+      LOCAL_NUMBER(operation->fi->fh),
+      bindings_buffer( operation->data, operation->size),
+      LOCAL_NUMBER(operation->size),
+      LOCAL_NUMBER(operation->offset),
+      callback
+    };
+    bindings.read->Call( 6, tmp );
+
+  }else if( operation->type == OP_RELEASE ){
+
+    Local<Value> tmp[] = {LOCAL_NUMBER(operation->ino), LOCAL_NUMBER(operation->fi->fh), callback};
+    bindings.release->Call( 3, tmp );
 
   }else{
     fprintf(stderr, "uv_handler => %s\n", "not implemented");
@@ -166,127 +332,130 @@ static void uv_handler(uv_async_t *handle){
 
 }
 
-static int hello_stat(fuse_ino_t ino, struct stat *stbuf){
-  fprintf(stderr, "%s\n", "stat");
-  stbuf->st_ino = ino;
-  switch (ino) {
-  case 1:
-    stbuf->st_mode = S_IFDIR | 0755;
-    stbuf->st_nlink = 2;
-    break;
-
-  case 2:
-    stbuf->st_mode = S_IFREG | 0444;
-    stbuf->st_nlink = 1;
-    stbuf->st_size = strlen(hello_str);
-    break;
-
-  default:
-    return -1;
-  }
-  return 0;
-}
-
-static void hello_ll_getattr(fuse_req_t req, fuse_ino_t ino, struct fuse_file_info *fi){
+static void ll_getattr(fuse_req_t req, fuse_ino_t ino, struct fuse_file_info *fi){
 
   struct operation_template operation;
 
-  operation.index = operation_count++;
   operation.type = OP_GETATTR;
   operation.req = &(req);
   operation.ino = ino;
   loop_async.data = &(operation);
 
+  pthread_mutex_lock(&mutex);
+  operation.index = operation_count++;
   operations_map[ operation.index ] = &(operation);
+  pthread_mutex_unlock(&mutex);
 
   uv_async_send(&loop_async);
+  pthread_mutex_lock(&mutex_ip);
 }
 
-static void hello_ll_lookup(fuse_req_t req, fuse_ino_t parent, const char *name){
-  fprintf(stderr, "%s => \n", "lookup");
-  struct fuse_entry_param e;
+static void ll_lookup(fuse_req_t req, fuse_ino_t parent, const char *name){
+  struct operation_template operation;
 
-  if (parent != 1 || strcmp(name, hello_name) != 0)
-    fuse_reply_err(req, ENOENT);
-  else {
-    memset(&e, 0, sizeof(e));
-    e.ino = 2;
-    e.attr_timeout = 1.0;
-    e.entry_timeout = 1.0;
-    hello_stat(e.ino, &e.attr);
+  operation.type = OP_LOOKUP;
+  operation.req = &(req);
+  operation.ino = parent;
+  operation.name = name;
+  loop_async.data = &(operation);
 
-    fuse_reply_entry(req, &e);
-  }
+  pthread_mutex_lock(&mutex);
+  operation.index = operation_count++;
+  operations_map[ operation.index ] = &(operation);
+  pthread_mutex_unlock(&mutex);
+
+  uv_async_send(&loop_async);
+  pthread_mutex_lock(&mutex_ip);
 }
 
-struct dirbuf {
-  char *p;
-  size_t size;
-};
+static void ll_readdir(fuse_req_t req, fuse_ino_t ino, size_t size, off_t off, struct fuse_file_info *fi){
+  struct operation_template operation;
 
-static void dirbuf_add(fuse_req_t req, struct dirbuf *b, const char *name, fuse_ino_t ino){
-  struct stat stbuf;
-  size_t oldsize = b->size;
-  b->size += fuse_add_direntry(req, NULL, 0, name, NULL, 0);
-  b->p = (char *) realloc(b->p, b->size);
-  memset(&stbuf, 0, sizeof(stbuf));
-  stbuf.st_ino = ino;
-  fuse_add_direntry(req, b->p + oldsize, b->size - oldsize, name, &stbuf,
-        b->size);
+  operation.type = OP_READDIR;
+  operation.req = &(req);
+  operation.size = size;
+  operation.offset = off;
+  operation.ino = ino;
+  operation.fi = fi;
+  loop_async.data = &(operation);
+
+  pthread_mutex_lock(&mutex);
+  operation.index = operation_count++;
+  operations_map[ operation.index ] = &(operation);
+  pthread_mutex_unlock(&mutex);
+
+  uv_async_send(&loop_async);
+  pthread_mutex_lock(&mutex_ip);
 }
 
-#define min(x, y) ((x) < (y) ? (x) : (y))
-
-static int reply_buf_limited(fuse_req_t req, const char *buf, size_t bufsize, off_t off, size_t maxsize){
-  if (off < bufsize)
-    return fuse_reply_buf(req, buf + off,
-              min(bufsize - off, maxsize));
-  else
-    return fuse_reply_buf(req, NULL, 0);
-}
-
-static void hello_ll_readdir(fuse_req_t req, fuse_ino_t ino, size_t size, off_t off, struct fuse_file_info *fi){
-  fprintf(stderr, "%s\n", "readdir");
-  (void) fi;
-
-  if (ino != 1)
-    fuse_reply_err(req, ENOTDIR);
-  else {
-    struct dirbuf b;
-
-    memset(&b, 0, sizeof(b));
-    dirbuf_add(req, &b, ".", 1);
-    dirbuf_add(req, &b, "..", 1);
-    dirbuf_add(req, &b, hello_name, 2);
-    reply_buf_limited(req, b.p, b.size, off, size);
-    free(b.p);
-  }
-}
-
-static void hello_ll_open(fuse_req_t req, fuse_ino_t ino, struct fuse_file_info *fi){
+static void ll_open(fuse_req_t req, fuse_ino_t ino, struct fuse_file_info *fi){
   fprintf(stderr, "%s\n", "open");
-  if (ino != 2)
-    fuse_reply_err(req, EISDIR);
-  else if ((fi->flags & 3) != O_RDONLY)
-    fuse_reply_err(req, EACCES);
-  else
-    fuse_reply_open(req, fi);
+
+  struct operation_template operation;
+
+  operation.type = OP_OPEN;
+  operation.req = &(req);
+  operation.ino = ino;
+  operation.fi = fi;
+  loop_async.data = &(operation);
+
+  pthread_mutex_lock(&mutex);
+  operation.index = operation_count++;
+  operations_map[ operation.index ] = &(operation);
+  pthread_mutex_unlock(&mutex);
+
+  uv_async_send(&loop_async);
+  pthread_mutex_lock(&mutex_ip);
 }
 
-static void hello_ll_read(fuse_req_t req, fuse_ino_t ino, size_t size, off_t off, struct fuse_file_info *fi){
+static void ll_release(fuse_req_t req, fuse_ino_t ino, struct fuse_file_info *fi){
+  struct operation_template operation;
+
+  operation.type = OP_RELEASE;
+  operation.req = &(req);
+  operation.ino = ino;
+  operation.fi = fi;
+  loop_async.data = &(operation);
+
+  pthread_mutex_lock(&mutex);
+  operation.index = operation_count++;
+  operations_map[ operation.index ] = &(operation);
+  pthread_mutex_unlock(&mutex);
+
+  uv_async_send(&loop_async);
+  pthread_mutex_lock(&mutex_ip);
+}
+
+static void ll_read(fuse_req_t req, fuse_ino_t ino, size_t size, off_t off, struct fuse_file_info *fi){
   fprintf(stderr, "%s\n", "read");
-  (void) fi;
 
-  assert(ino == 2);
-  reply_buf_limited(req, hello_str, strlen(hello_str), off, size);
+  struct operation_template operation;
+  char buf[ size ];
+
+  operation.type = OP_READ;
+  operation.req = &(req);
+  operation.size = size;
+  operation.offset = off;
+  operation.ino = ino;
+  operation.data = buf;
+  operation.fi = fi;
+  loop_async.data = &(operation);
+
+  pthread_mutex_lock(&mutex);
+  operation.index = operation_count++;
+  operations_map[ operation.index ] = &(operation);
+  pthread_mutex_unlock(&mutex);
+
+  uv_async_send(&loop_async);
+  pthread_mutex_lock(&mutex_ip);
 }
 
-static struct fuse_lowlevel_ops hello_ll_oper = {};
+static struct fuse_lowlevel_ops ll_oper = {};
 
 static void *fuse_thread(void *path){
 
   /*
-  //hello_ll_oper.release = hello_ll_release;
+  //ll_oper.release = ll_release;
   */
   //char **argv = [ "fuse-bindings", path ];
 
@@ -320,7 +489,7 @@ static void *fuse_thread(void *path){
     goto err_out1;
   }*/
 
-  se = fuse_session_new(&args, &hello_ll_oper, sizeof(hello_ll_oper), NULL);
+  se = fuse_session_new(&args, &ll_oper, sizeof(ll_oper), NULL);
 
   /*
   if (se == NULL)
@@ -339,7 +508,7 @@ static void *fuse_thread(void *path){
   int ret = fuse_session_loop(se);
     fprintf(stderr, "%s\n", "fin bucle");
 
-  
+
   fuse_session_unmount(se);
   /*
 
@@ -357,7 +526,6 @@ NAN_METHOD(SetCallback) {
 }
 
 NAN_METHOD(Mount) {
-  fprintf(stderr, "%s => %d\n", "nan mount", std::this_thread::get_id() );
 
   if(!info[0]->IsString()){
     return Nan::ThrowError("mnt must be a string");
@@ -365,11 +533,13 @@ NAN_METHOD(Mount) {
 
   //ToDo -> Path
   Local<Object> operations = info[1].As<Object>();
-  
+
+  bindings.open    = LOOKUP_CALLBACK(operations, "open");
+  bindings.read    = LOOKUP_CALLBACK(operations, "read");
   bindings.getattr = LOOKUP_CALLBACK(operations, "getattr");
-  /*bindings.stat    = LOOKUP_CALLBACK(operations, "stat");
+  bindings.lookup  = LOOKUP_CALLBACK(operations, "lookup");
   bindings.readdir = LOOKUP_CALLBACK(operations, "readdir");
-  bindings.lookup  = LOOKUP_CALLBACK(operations, "lookup");*/
+  bindings.release = LOOKUP_CALLBACK(operations, "release");
 
   loop = uv_default_loop();
   uv_async_init(loop, &loop_async, uv_handler);
@@ -379,17 +549,20 @@ NAN_METHOD(Mount) {
   pthread_t thread_id;
   pthread_attr_t thread_attr;
 
+  pthread_mutex_init(&mutex, NULL);
+  pthread_mutex_init(&mutex_ip, NULL);
   pthread_attr_init(&thread_attr);
   pthread_attr_setdetachstate(&thread_attr, PTHREAD_CREATE_JOINABLE);
   pthread_create(&thread_id, &thread_attr, &fuse_thread, &path);
 }
 
 void Init(Handle<Object> exports) {
-  hello_ll_oper.lookup  = hello_ll_lookup;
-  hello_ll_oper.getattr = hello_ll_getattr;
-  hello_ll_oper.open    = hello_ll_open;
-  hello_ll_oper.read    = hello_ll_read;
-  hello_ll_oper.readdir = hello_ll_readdir;
+  ll_oper.getattr = ll_getattr;
+  ll_oper.lookup  = ll_lookup;
+  ll_oper.open    = ll_open;
+  ll_oper.read    = ll_read;
+  ll_oper.readdir = ll_readdir;
+  ll_oper.release = ll_release;
 
   exports->Set(LOCAL_STRING("setCallback"), Nan::New<FunctionTemplate>(SetCallback)->GetFunction());
   exports->Set(LOCAL_STRING("mount"), Nan::New<FunctionTemplate>(Mount)->GetFunction());
